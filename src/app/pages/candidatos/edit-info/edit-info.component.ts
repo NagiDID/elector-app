e3b558021c44d11fe21e4972d278e94962248562
NagiDID <claroleonardo21@gmail.com>
2024-02-27 17:13:36 -0500

    Terminé feature de edición y registro de candidatos

--- src/app/pages/candidatos/edit-info/edit-info.component.ts
+++ src/app/pages/candidatos/edit-info/edit-info.component.ts
@@ -1,7 +1,7 @@
-import { Component, input, signal} from '@angular/core';
+import { Component, input, signal, ɵPendingTasks} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { candidate } from '../../../models/tags.model';
-import { findIndex } from 'rxjs';
+import { elementAt, findIndex } from 'rxjs';
 
 @Component({
   selector: 'app-edit-info',
@@ -12,20 +12,10 @@ import { findIndex } from 'rxjs';
 })
 export class EditInfoComponent {
 
-  
+  displayType = signal<string>('display: none');
 
   candidatos = signal<candidate[]>([
-
   ]);
-
-  addNewCandidate (event: Event) {
-    const newCandidate = {
-      avatar: 'https://i.imgur.com/e8buxpa.jpeg',
-      number: 2,
-      name: 'Jhon Doe',
-    }
-    this.candidatos.update((candidatos) => [...candidatos, newCandidate]);
-  }
 
   deleteCandidate (index:number) {
     this.candidatos.update ((candidatos) => candidatos.filter((candidatos, position) => position !== index))
@@ -53,4 +43,56 @@ export class EditInfoComponent {
     this.candidatos.update ((candidatos) => [...candidatos, newCandidate]);
   }
   
+  popUpEditModule(event:Event, index:number) {
+    const stateOn = 'display: flex;';
+    this.displayType.set (stateOn);
+    this.indexReceived.set (index);
+    this.currentName.set(String(this.candidatos().at(index)?.name))
+    this.currentNumber.set(Number(this.candidatos().at(index)?.number))
+  }
+
+  closePopUp(event:Event) {
+    const stateOff = 'display: none;'
+    this.displayType.set (stateOff);
+  }
+
+  currentName =signal<string>('Default')
+  currentNumber =signal<number>(0)
+  indexReceived = signal<number>(0)
+
+  changeName (event:Event) {
+    const inputName = event.target as HTMLInputElement;
+    this.newName.set(inputName.value);
+  }
+
+  changeNumber (event:Event) {
+    const inputNumber = event.target as HTMLInputElement;
+    this.newNumber.set(parseInt(inputNumber.value));
+  }
+
+  newName = signal<string>('default');
+  newNumber = signal<number>(1);
+
+  editCandidateinfo (event:Event) {
+    const stateOff = 'display: none;'
+    this.displayType.set (stateOff);
+    this.candidatos.update((candidatos) => {
+      return  candidatos.map((candidato, position) => {
+        if (position === this.indexReceived()) {
+          return {
+            ...candidato,
+            number: this.newNumber(),
+            name: this.newName(),
+          }
+        }
+        return candidato;
+      })
+    }
+    )
+  }
+
+  // alertSign(event:Event) {
+  //   const currentName = this.candidatos().at(this.indexReceived())?.name;
+  //   alert(currentName);
+  // }
 }
