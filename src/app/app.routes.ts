be873248f53a2c05b25ebc23d58786692267a119
NagiDID <claroleonardo21@gmail.com>
2024-04-25 16:27:27 -0500

    Agrego componente registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ConfigComponent } from './pages/config/config.component';
 import { TarjetonEditComponent } from './pages/config/tarjeton-edit/tarjeton-edit.component';
 import { AdminScreenComponent } from './pages/admin-screen/admin-screen.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
 export const routes: Routes = [
     {
@@ -17,4 +18,9 @@ export const routes: Routes = [
         path:'modoAdmin',
         component: AdminScreenComponent,
     },
+    {
+        path:'registro',
+        component: RegistroComponent,
+    },
+    
 ];
